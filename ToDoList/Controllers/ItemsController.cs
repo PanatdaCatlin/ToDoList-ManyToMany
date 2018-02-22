bf2577eb417594178ad5a7d26a7fd39f877d6cf8
Jamie Pittak <charrawr456@gmail.com>
2018-02-21 21:03:00 -0800

    add todolist mvc web app and testing

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+        [HttpGet("/categories/{categoryId}/items/new")]
+        public ActionResult CreateForm(int categoryId)
+        {
+          Dictionary<string, object> model = new Dictionary<string, object>();
+          Category category = Category.Find(categoryId);
+          return View(category);
+        }
+        [HttpGet("/categories/{categoryId}/items/{itemId}")]
+        public ActionResult Details(int categoryId, int itemId)
+        {
+          Item item = Item.Find(itemId);
+          Dictionary<string, object> model = new Dictionary<string, object>();
+          Category category = Category.Find(categoryId);
+          model.Add("item", item);
+          model.Add("category", category);
+          return View(item);
+        }
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
+        [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
+        {
+            Item item = Item.Find(id);
+            return View(item);
+        }
+    }
+}
